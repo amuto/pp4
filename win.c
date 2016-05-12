09c9831486b3dea50279ae11420b452a51f21d11
MUTO Akihiro <b608428@bes-flr1.(none)>
2016-05-12 12:25:43 +0900

    widget wo hyouji suru

--- win.c
+++ win.c
@@ -3,11 +3,40 @@
 
 int main(int argc, char **argv)
 {
-  GtkWidget *win;
+  GtkWidget *win, *l1, *l2, *l3, *l4, *box1, *box2, *box3;
   gtk_init(&argc, &argv);
 
   /* make a window */
   win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+  box1 = gtk_vbox_new(FALSE,10);
+  gtk_container_add(GTK_CONTAINER(win), box1);
+ 
+  l1 = gtk_label_new("Label1");
+  gtk_box_pack_start(GTK_BOX(box1), l1, FALSE, TRUE, 10);
+
+  l2 = gtk_label_new("Label2");
+  gtk_box_pack_start(GTK_BOX(box1), l2, TRUE, FALSE, 10);
+ 
+  box2 = gtk_vbox_new(FALSE,10);
+  gtk_box_pack_start(GTK_BOX(box1), box2, FALSE, TRUE, 10);
+ 
+  box3 = gtk_hbox_new(FALSE,10);
+  gtk_box_pack_start(GTK_BOX(box1), box3, FALSE, TRUE, 10);
+
+  l3 = gtk_label_new("Label3");
+  gtk_box_pack_start(GTK_BOX(box3), l3, TRUE, FALSE, 10);
+ 
+  l4 = gtk_label_new("Label4");
+  gtk_box_pack_start(GTK_BOX(box3), l4, FALSE, TRUE, 10);
+
+  gtk_widget_show(box1);
+  gtk_widget_show(l1);
+  gtk_widget_show(l2);
+  gtk_widget_show(box2);
+  gtk_widget_show(box3);
+  gtk_widget_show(l3);
+  gtk_widget_show(l4);
   gtk_widget_show(win);
 
   gtk_main();
